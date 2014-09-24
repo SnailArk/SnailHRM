3b6c67a925cd7a1ace429ce673a6634a82986221
USArora <usarora@snailark.com>
2014-09-24 14:16:00 +0530

    Renamed the initial script

--- SnailHRM/dbscript/snailhrm.sql
+++
@@ -1,8 +0,0 @@
-create database if not exists SNAILHRM;
-
-create table `SNAILHRM`.`DEPARTMENT` (
-  `ID` integer unsigned not null auto_increment,
-  `DEPARTMENT_NAME` varchar(45) not null,
-  `NOTES` varchar(1000) not null,
-  primary key (`ID`)
-);
